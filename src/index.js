b340491dbc38426c24b4259d9fd07e49656f32db
Aryan Mishra <aryanmishra12112003@gmail.com>
2025-07-07 22:00:31 +0530

    util code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path:'./.env'
 })
 
-connectDB()
+connectDB()// async functions hamesa promise return karte toh ya fir then catch karo ya toh ek aur await lagao
+.then(()=>{
+    app.on("Error",(error)=>{
+        console.log("ERRR :",error)
+        throw error
+    })
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(` Server is running at ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("Mongo DB coonection failed !!", err)
+})
